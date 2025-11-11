671aa8e66ad72a40cf82c0347fe6a8ec31d88f86
Joab Jabulani Longwe <74425802+Jabu2000@users.noreply.github.com>
2025-11-11 03:11:34 +0200

    Update BASE_URL to production URL

--- server/seeds/seedStores.js
+++ server/seeds/seedStores.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import Store from "../models/Store.js";
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:4000";
+const BASE_URL = process.env.BASE_URL || "https://adventuretimecpt.onrender.com";
 
 const data = [
   // --- Vintage ---

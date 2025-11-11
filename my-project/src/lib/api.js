1d3b1fde3a62cd91b6d1ed6c4603de3267c02b91
Jabulani <joabjabulanilongwe@gmail.com>
2025-11-11 04:50:04 +0200

    deployment-commit

--- my-project/src/lib/api.js
+++ my-project/src/lib/api.js
@@ -1,4 +1,4 @@
-export const API = import.meta.env.VITE_API_BASE || "http://localhost:4000/api";
+export const API = import.meta.env.VITE_API_BASE || "http://localhost:4000";
 
 export async function apiFetch(path, opts = {}) {
   const res = await fetch(`${API}${path}`, {
